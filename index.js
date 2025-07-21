c4586679429b685251386f1f5eabb822654c713c
Saloomaa <salmazah07@gmail.com>
2025-07-21 23:33:21 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+// Import express, axios, and body-parser modules
+import express from "express";
+import bodyParser from "body-parser";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Resolve __dirname for ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Create an express application
+const app = express();
+
+//declare a port to use for the server
+//const port = 3000; //for local testing
+
+// Set up EJS as the view engine
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views")); // Ensure the views folder is correctly located
+
+// Assign the public folder for static assets (CSS, images, etc.)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Middleware to parse request bodies
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Define the home route
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+// Export the app for Vercel
+export default app; //for vercel deployment
+
+
+//listening to the declared port locally
+//app.listen(port,()=>{
+//console.log("Listening to the port : "+port);
+//});
---
+++ public/js/main.js
@@ -0,0 +1,13 @@
+
+//initialized a typed object from a Typed module for text typing animation
+var typed = new Typed('#animatedSubtitleTyping', {
+    strings: ['ORGANISATION.', 'PROJECT.','COMPANY.','EDUCATION.'],
+    typeSpeed: 100,
+    loop: true,
+    backSpeed: 30,
+    backDelay: 1500,
+    
+  });
+
+
+
