62869bad11a232d68c200f98d40047bb8d38b122
Saloomaa <salmazah07@gmail.com>
2025-07-23 20:03:58 +0100

    add projects,picture

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,7 @@
 
 //initialized a typed object from a Typed module for text typing animation
 var typed = new Typed('#animatedSubtitleTyping', {
-    strings: ['ORGANISATION.', 'PROJECT.','COMPANY.','EDUCATION.'],
+    strings: ['ORGANISATION.', 'PROJECT.','COMPANY.','EDUCATION.',' STARTUPS','BUSINESSES',' BRANDS','COMMUNITIES'],
     typeSpeed: 100,
     loop: true,
     backSpeed: 30,
